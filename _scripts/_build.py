770d8d05189d9690c81924cec5ac5e5e4475d6e2
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-06-02 14:42:05 -0400

    try build config (might do nothing for now)

--- _scripts/_build.py
+++ _scripts/_build.py
@@ -3,13 +3,6 @@ import sys
 
 from . import ROOT, PY_SRC, _run, PY, DIST
 
-"""
-|-
-cd packages/ipywxyz \
-&& python setup.py sdist --dist-dir $PROJECT_DIR/dist/sdist \
-&& cd $PROJECT_DIR \
-&& conda-build -c conda-forge --output-folder dist/conda-bld conda.recipe
-"""
 
 if __name__ == "__main__":
     for pkg in PY_SRC.glob("wxyz_*"):
