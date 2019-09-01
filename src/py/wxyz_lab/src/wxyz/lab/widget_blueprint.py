708e600d8aa847f8749cb261df38be6c6a88a76c
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-09-01 19:02:17 -0400

    try out some blueprint (icons, buttons)

---
+++ src/py/wxyz_lab/src/wxyz/lab/widget_blueprint.py
@@ -0,0 +1,43 @@
+""" Text editing widgets
+"""
+# pylint: disable=too-many-ancestors
+from .base import LabBase, T, W
+
+
+@W.register
+class Icon(LabBase, W.DOMWidget):
+    """ A Blueprint icon
+    """
+
+    _model_name = T.Unicode("IconModel").tag(sync=True)
+    _view_name = T.Unicode("IconView").tag(sync=True)
+
+    icon = T.Unicode().tag(sync=True)
+    color = T.Unicode().tag(sync=True)
+    icon_size = T.Int(16).tag(sync=True)
+
+    def __init__(self, icon=None, *args, **kwargs):
+        if icon:
+            kwargs["icon"] = icon
+        super().__init__(*args, **kwargs)
+
+
+@W.register
+class Button(LabBase, W.Button):
+    _model_name = T.Unicode("ButtonModel").tag(sync=True)
+    _view_name = T.Unicode("ButtonView").tag(sync=True)
+
+    icon = T.Union([T.Unicode(), T.Instance(Icon)]).tag(
+        sync=True, **W.widget_serialization)
+    icon_right = T.Union([T.Unicode(), T.Instance(Icon)]).tag(
+        sync=True, **W.widget_serialization)
+
+    large = T.Bool(False).tag(sync=True)
+    loading = T.Bool(False).tag(sync=True)
+    minimal = T.Bool(False).tag(sync=True)
+    small = T.Bool(False).tag(sync=True)
+    active = T.Bool(False).tag(sync=True)
+
+    @T.validate('icon')
+    def _validate_icon(self, proposal):
+        return proposal['value']
