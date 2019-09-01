09ddffed81b974d33b905cf6d7898709ca50dfae
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-09-01 19:14:08 -0400

    linting

--- src/py/wxyz_lab/src/wxyz/lab/widget_blueprint.py
+++ src/py/wxyz_lab/src/wxyz/lab/widget_blueprint.py
@@ -16,21 +16,25 @@ class Icon(LabBase, W.DOMWidget):
     color = T.Unicode().tag(sync=True)
     icon_size = T.Int(16).tag(sync=True)
 
-    def __init__(self, icon=None, *args, **kwargs):
-        if icon:
-            kwargs["icon"] = icon
-        super().__init__(*args, **kwargs)
+    def __init__(self, icon=None, **kwargs):
+        kwargs["icon"] = icon
+        super().__init__(**kwargs)
 
 
 @W.register
 class Button(LabBase, W.Button):
+    """ A Blueprint button
+    """
+
     _model_name = T.Unicode("ButtonModel").tag(sync=True)
     _view_name = T.Unicode("ButtonView").tag(sync=True)
 
     icon = T.Union([T.Unicode(), T.Instance(Icon)]).tag(
-        sync=True, **W.widget_serialization)
+        sync=True, **W.widget_serialization
+    )
     icon_right = T.Union([T.Unicode(), T.Instance(Icon)]).tag(
-        sync=True, **W.widget_serialization)
+        sync=True, **W.widget_serialization
+    )
 
     large = T.Bool(False).tag(sync=True)
     loading = T.Bool(False).tag(sync=True)
@@ -38,6 +42,6 @@ class Button(LabBase, W.Button):
     small = T.Bool(False).tag(sync=True)
     active = T.Bool(False).tag(sync=True)
 
-    @T.validate('icon')
+    @T.validate("icon")
     def _validate_icon(self, proposal):
-        return proposal['value']
+        return proposal["value"]
