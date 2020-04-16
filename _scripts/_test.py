f660b03cca23bd6417ccd9e5bf3a82a61908630d
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-04-16 19:42:18 -0400

    importnb 0.6.1 fixes py38 issues

--- _scripts/_test.py
+++ _scripts/_test.py
@@ -11,12 +11,6 @@ NOTEBOOKS_TO_TEST = [
     i for i in sorted(IPYNB.rglob("*.ipynb")) if "ipynb_checkpoint" not in str(i)
 ]
 
-if sys.version_info >= (3, 8):
-    NOTEBOOKS_TO_TEST = [
-        i for i in NOTEBOOKS_TO_TEST if "importnb" not in i.read_bytes().decode("utf-8")
-    ]
-
-
 @pytest.mark.parametrize("name,ipynb", [[i.name, i] for i in NOTEBOOKS_TO_TEST])
 def test_notebook(name, ipynb):
     _run([PY, "-m",  "nbconvert", "--output-dir", IPYNB_HTML, "--execute", ipynb])
