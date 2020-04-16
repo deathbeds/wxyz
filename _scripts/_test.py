1b16ee13eb14a852334304179e85ecefcde44389
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-04-16 17:10:26 -0400

    use pytest, xdist

--- _scripts/_test.py
+++ _scripts/_test.py
@@ -1,19 +1,29 @@
-from . import IPYNB, IPYNB_HTML, PY, _run
-
 import sys
 
+from . import IPYNB, IPYNB_HTML, PY, _run
+
+import pytest
+import multiprocessing
+
+CPU_COUNT = multiprocessing.cpu_count()
+
+NOTEBOOKS_TO_TEST = [
+    i for i in sorted(IPYNB.rglob("*.ipynb")) if "ipynb_checkpoint" not in str(i)
+]
+
+if sys.version_info >= (3, 8) and "importnb" in ipynb.read_bytes().decode("utf-8"):
+    NOTEBOOKS_TO_TEST = [
+        i for i in NOTEBOOKS_TO_TEST if "importnb" not in ipynb.read_bytes().decode("utf-8")
+    ]
+
+
+@pytest.mark.parametrize("name,ipynb", [[i.name, i] for i in NOTEBOOKS_TO_TEST])
+def test_notebook(name, ipynb):
+    if sys.version_info >= (3, 8) and "importnb" in ipynb.read_bytes().decode("utf-8"):
+        return
+
+    _run([PY, "-m",  "nbconvert", "--output-dir", IPYNB_HTML, "--execute", ipynb])
+
+
 if __name__ == "__main__":
-    errors = 0
-
-    for ipynb in sorted(IPYNB.rglob("*.ipynb")):
-        if "ipynb_checkpoint" in str(ipynb):
-            continue
-        if sys.version_info >= (3, 8) and "importnb" in ipynb.read_bytes().decode("utf-8"):
-            continue
-
-        try:
-            _run([PY, "-m",  "nbconvert", "--output-dir", IPYNB_HTML, "--execute", ipynb])
-        except:
-            errors += 1
-
-    sys.exit(errors)
+    pytest.main([__file__, "-vv", "-n", str(CPU_COUNT)])
