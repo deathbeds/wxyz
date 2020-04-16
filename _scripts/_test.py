8af9b7d120e862b47f5c2e7de1e69a4e91a63109
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-04-16 14:58:59 -0400

    skip importnb notebooks on py3.8

--- _scripts/_test.py
+++ _scripts/_test.py
@@ -1,7 +1,11 @@
 from . import IPYNB, IPYNB_HTML, PY, _run
 
+import sys
+
 if __name__ == "__main__":
     for ipynb in IPYNB.rglob("*.ipynb"):
         if "ipynb_checkpoint" in str(ipynb):
+            continue
+        if sys.version_info >= (3, 8) and "importnb" in ipynb.read_text():
             continue
         _run([PY, "-m",  "nbconvert", "--output-dir", IPYNB_HTML, "--execute", ipynb])
