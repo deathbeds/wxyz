950579abe88e300b2c3ee36ba4e4ecf857222bb4
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-04-16 16:57:26 -0400

    run all the notebooks, error with number that failed

--- _scripts/_test.py
+++ _scripts/_test.py
@@ -3,9 +3,17 @@ from . import IPYNB, IPYNB_HTML, PY, _run
 import sys
 
 if __name__ == "__main__":
-    for ipynb in IPYNB.rglob("*.ipynb"):
+    errors = 0
+
+    for ipynb in sorted(IPYNB.rglob("*.ipynb")):
         if "ipynb_checkpoint" in str(ipynb):
             continue
-        if sys.version_info >= (3, 8) and "importnb" in ipynb.read_text():
+        if sys.version_info >= (3, 8) and "importnb" in ipynb.read_bytes().decode("utf-8"):
             continue
-        _run([PY, "-m",  "nbconvert", "--output-dir", IPYNB_HTML, "--execute", ipynb])
+
+        try:
+            _run([PY, "-m",  "nbconvert", "--output-dir", IPYNB_HTML, "--execute", ipynb])
+        except:
+            errors += 1
+
+    sys.exit(errors)
