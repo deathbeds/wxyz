9127468087e169225e09a95ae563ac83937662d5
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-04-16 20:38:25 -0400

    package notebooks

---
+++ src/py/wxyz_notebooks/src/wxyz/notebooks/_version.py
@@ -0,0 +1,4 @@
+""" some constants
+"""
+# pylint: disable=invalid-name
+__version__ = "0.4.0"
---
+++ src/py/wxyz_notebooks/src/wxyz/notebooks/tests/test_nbconvert.py
@@ -0,0 +1,16 @@
+import sys
+
+from . import IPYNB, IPYNB_HTML, PY, _run
+
+import pytest
+import multiprocessing
+
+CPU_COUNT = multiprocessing.cpu_count()
+
+NOTEBOOKS_TO_TEST = [
+    i for i in sorted(IPYNB.rglob("*.ipynb")) if "ipynb_checkpoint" not in str(i)
+]
+
+@pytest.mark.parametrize("name,ipynb", [[i.name, i] for i in NOTEBOOKS_TO_TEST])
+def test_notebook(name, ipynb):
+    _run([PY, "-m",  "nbconvert", "--output-dir", IPYNB_HTML, "--execute", ipynb])
