1a3f8bb5a374d09059cec0837025dfe31745a16c
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-04-17 11:28:24 -0400

    clean up lint

--- dodo.py
+++ dodo.py
@@ -3,13 +3,19 @@
 import site
 import sys
 from pathlib import Path
+from subprocess import call
 
 from doit.tools import result_dep
 
+PY = Path(sys.executable)
+
 DODO = Path(__file__)
 ROOT = DODO.parent
+DIST = ROOT / "dist"
 CI = ROOT / "ci"
+
 SRC = ROOT / "src"
+
 TS_SRC = SRC / "ts"
 PY_SRC = SRC / "py"
 PY_SETUP = [*PY_SRC.glob("*/setup.py")]
@@ -18,7 +24,7 @@ SITE_PKGS = Path(site.getsitepackages()[0])
 PY_LINT_CMDS = [
     ["isort", "-rc"],
     ["black", "--quiet"],
-    ["flake8"],
+    ["flake8", "--max-line-length", "88"],
     ["pylint"],
 ]
 
@@ -37,8 +43,13 @@ def task_setup():
         actions=[["jlpm", "--prefer-offline"], ["jlpm", "lerna", "bootstrap"]],
     )
 
-    for setup_py in PY_SETUP:
+    for i, setup_py in enumerate(PY_SETUP):
         pkg = setup_py.parent
+
+        uptodate = {}
+
+        if i:
+            uptodate["uptodate"] = [result_dep(f"setup:py_{PY_SETUP[i-1].parent.name}")]
 
         yield dict(
             name=f"py_{pkg.name}",
@@ -46,7 +57,7 @@ def task_setup():
             targets=[SITE_PKGS / f"{pkg.name}.egg-link".replace("_", "-")],
             actions=[
                 [
-                    sys.executable,
+                    PY,
                     "-m",
                     "pip",
                     "install",
@@ -56,6 +67,7 @@ def task_setup():
                     "--no-deps",
                 ]
             ],
+            **uptodate,
         )
 
 
@@ -81,8 +93,7 @@ def task_lint():
     )
 
     groups = {
-        setup_py.parent.name: sorted(setup_py.parent.rglob("*.py"))
-        for setup_py in PY_SETUP
+        i.parent.name: [i, *sorted((i.parent / "src").rglob("*.py"))] for i in PY_SETUP
     }
 
     groups["misc"] = [DODO]
@@ -90,3 +101,24 @@ def task_lint():
     for label, files in groups.items():
         actions = [cmd + files for cmd in PY_LINT_CMDS]
         yield dict(name=label, file_dep=files, actions=actions, **uptodate)
+
+
+def _one_pydist(pkg, file_dep, output):
+    name = f"{output}_{pkg.name}"
+    args = [PY, "setup.py", output, "--dist-dir", DIST / output]
+    actions = [lambda: call(args, cwd=pkg) == 0]
+    return dict(name=name, file_dep=file_dep, actions=actions)
+
+
+def task_pydist():
+    """ build python release artifacts
+    """
+    for setup_py in PY_SETUP:
+        pkg = setup_py.parent
+        file_dep = [
+            setup_py,
+            pkg / "setup.cfg",
+            *sorted((pkg / "src").rglob("*.py")),
+        ]
+        for output in ["sdist", "bdist_wheel"]:
+            yield _one_pydist(pkg, file_dep, output)
