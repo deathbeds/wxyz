f7587c947f9487632e553080cd93a3fd752417c3
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-12-16 17:20:42 -0600

    more ci cleanup

--- .github/win.bat
+++ .github/win.bat
@@ -1,4 +1,4 @@
 ::rem this really shouldn't be used locally
 call %CONDA%/Scripts/activate.bat ./envs/_base
 
-doit setup_py_ci nbtest robot || doit setup_py_ci nbtest robot || exit 1
+doit setup_py nbtest robot || doit setup_py nbtest robot || exit 1
