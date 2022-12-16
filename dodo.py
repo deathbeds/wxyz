930b501cbab9e806b9ede54837c1938e0dc9c398
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-12-16 17:41:22 -0600

    work on setup

--- dodo.py
+++ dodo.py
@@ -175,7 +175,7 @@ def task_setup_py():
     """setup python packages"""
     if P.RUNNING_IN_CI:
 
-        return dict(
+        yield dict(
             file_dep=[*P.WHEELS.values()],
             targets=[P.OK / "setup_py", P.OK / "setup_lab"],
             actions=[
@@ -195,53 +195,54 @@ def task_setup_py():
                 U.okit("setup_lab"),
             ],
         )
+    else:
 
-    def write_reqs_txt():
-        """write out a requirements file so everything can be installed in one go"""
-        P.BUILD.exists() or P.BUILD.mkdir()
-        P.PY_DEV_REQS.write_text(
-            "\n".join(
-                [f"-e {p.parent.relative_to(P.ROOT)}" for p in P.ALL_PYPROJECT_TOML]
+        def write_reqs_txt():
+            """write out a requirements file so everything can be installed in one go"""
+            P.BUILD.exists() or P.BUILD.mkdir()
+            P.PY_DEV_REQS.write_text(
+                "\n".join(
+                    [f"-e {p.parent.relative_to(P.ROOT)}" for p in P.ALL_PYPROJECT_TOML]
+                )
             )
+
+        yield dict(
+            name="reqs_txt",
+            targets=[P.PY_DEV_REQS],
+            file_dep=[*P.ALL_PYPROJECT_TOML],
+            actions=[write_reqs_txt],
         )
 
-    yield dict(
-        name="reqs_txt",
-        targets=[P.PY_DEV_REQS],
-        file_dep=[*P.ALL_PYPROJECT_TOML],
-        actions=[write_reqs_txt],
-    )
-
-    yield dict(
-        name="pip",
-        file_dep=[P.PY_DEV_REQS, *P.TS_D_PACKAGE_JSON.values()],
-        targets=[P.OK / "setup_py"],
-        actions=[
-            U.okit("setup_py", remove=True),
-            [
-                *P.PIP,
-                "install",
-                "--no-deps",
-                "--ignore-installed",
-                "-r",
-                P.PY_DEV_REQS,
+        yield dict(
+            name="pip",
+            file_dep=[P.PY_DEV_REQS, *P.TS_D_PACKAGE_JSON.values()],
+            targets=[P.OK / "setup_py"],
+            actions=[
+                U.okit("setup_py", remove=True),
+                [
+                    *P.PIP,
+                    "install",
+                    "--no-deps",
+                    "--ignore-installed",
+                    "-r",
+                    P.PY_DEV_REQS,
+                ],
+                [*P.PIP, "freeze"],
+                [*P.PIP, "check"],
+                U.okit("setup_py"),
             ],
-            [*P.PIP, "freeze"],
-            [*P.PIP, "check"],
-            U.okit("setup_py"),
-        ],
-    )
+        )
 
-    yield dict(
-        name="lab",
-        file_dep=[P.PY_DEV_REQS, P.OK / "setup_py"],
-        targets=[P.OK / "setup_lab"],
-        actions=[
-            U.okit("setup_lab", remove=True),
-            ["jupyter", "labextension", "list"],
-            U.okit("setup_lab"),
-        ],
-    )
+        yield dict(
+            name="lab",
+            file_dep=[P.PY_DEV_REQS, P.OK / "setup_py"],
+            targets=[P.OK / "setup_lab"],
+            actions=[
+                U.okit("setup_lab", remove=True),
+                ["jupyter", "labextension", "list"],
+                U.okit("setup_lab"),
+            ],
+        )
 
 
 def _make_linters(label, files):
